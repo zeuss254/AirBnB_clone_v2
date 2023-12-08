b69af86ba0620843ee47a08233dedd05941c9b6b
zeuss254 <ckarimi676@gmail.com>
2023-12-08 17:16:55 +0000

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ from fabric.api import local, put, run, env
 from datetime import datetime
 
 env.user = 'ubuntu'
-env.hosts = ['35.227.35.75', '100.24.37.33']
+env.hosts = ['54.236.12.192', '54.84.7.55']
 
 
 def do_pack():
